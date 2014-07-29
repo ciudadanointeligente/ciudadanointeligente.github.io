a8aef2f15acfc472fed6fc2fc752cbb1d1a92cd8
camargozzini <ca.margozzini@gmail.com>
2014-07-29 18:29:26 -0400

    WIP: Post de proyectos e iconos pilares.

---
+++ _posts/2014-06-25-congresoabierto.markdown
@@ -0,0 +1,11 @@
+---
+layout: post
+title:  "Congreso Abierto"
+date:   2014-06-25 14:46:22
+categories: project
+img: img/3.jpg
+thumb: img/thumbs/3.jpg
+description: Descripción sobre congreso abierto.
+url: http://congresoabierto.cl/
+estado: activo
+---
--- js/grid.js
+++ js/grid.js
@@ -344,7 +344,7 @@ var Grid = (function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
 			this.$description = $( '<p></p>' );
-			this.$href = $( '<a href="#">Visit website</a>' );
+			this.$href = $( '<a href="#">Ir al sitio</a>' );
 			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
