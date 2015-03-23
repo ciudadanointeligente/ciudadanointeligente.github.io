0a3b76af07bfb1a4ac5808c20abb8e3dc675cc3b
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2015-03-23 16:09:28 -0300

    code formatting

--- js/main.js
+++ js/main.js
@@ -1,52 +1,43 @@
-(function ($) {
+(function($) {
     "use strict";
-
     ///////////////////////////////////////////////////// Your
     var venueAddress = "Liber Bernardo O'Higgins 227, Santiago"; // Venue
     var placeName = "Centro Gabriela Mistral";
     /////////////////////////////////////////////////// Adress
-
     var fn = {
+        // Launch Functions
+        Launch: function() {
+            fn.MenuSticky();
+            fn.Navigation();
+        },
+        // Sticky Menu
+        MenuSticky: function() {
+            var menu = document.querySelector('#menu'),
+                origOffsetY = menu.offsetTop + 100;
 
-            // Launch Functions
-            Launch: function () {
-                    fn.MenuSticky();
-                    fn.Navigation();
-                },
-
-            // Sticky Menu
-            MenuSticky: function () {
-                var menu = document.querySelector('#menu'),
-                    origOffsetY = menu.offsetTop + 100;
-                function scroll() {
-                    if ($(window).scrollTop() >= origOffsetY) {
-                        $('#menu').addClass('sticky');
-                        $('#menu').removeClass('fixed');
-                    } else {
-                        $('#menu').removeClass('sticky');
-                        $('#menu').addClass('fixed');
-                    }
+            function scroll() {
+                if ($(window).scrollTop() >= origOffsetY) {
+                    $('#menu').addClass('sticky');
+                    $('#menu').removeClass('fixed');
+                } else {
+                    $('#menu').removeClass('sticky');
+                    $('#menu').addClass('fixed');
                 }
-                document.onscroll = scroll;
-            },
-
-            // One Page Navigation
-            Navigation: function () {
-                $('#menu').onePageNav({
-                    currentClass: 'current',
-                    scrollSpeed: 500,
-                    scrollOffset: 60,
-                    scrollThreshold: 0.2,
-                    easing: 'swing'
-                });
-            },
-
-
-
-            };
-
-        $(document).ready(function () {
-            fn.Launch();
-        });
-
-    })(jQuery);
+            }
+            document.onscroll = scroll;
+        },
+        // One Page Navigation
+        Navigation: function() {
+            $('#menu').onePageNav({
+                currentClass: 'current',
+                scrollSpeed: 500,
+                scrollOffset: 60,
+                scrollThreshold: 0.2,
+                easing: 'swing'
+            });
+        },
+    };
+    $(document).ready(function() {
+        fn.Launch();
+    });
+})(jQuery);
